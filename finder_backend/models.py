c229c27c590d88cfa776d605e4c2696551927c22
Sriram Iyer <ramiyer1998@gmail.com>
2019-07-13 12:32:01 -0400

    Make basic model for flights

--- finder_backend/models.py
+++ finder_backend/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Flight(models.Model):
+    origin = models.CharField(max_length=3)
+    destination = models.CharField(max_length=3)
+    departure_date = models.CharField(max_length=10)
+    return_date = models.CharField(max_length=10, blank=True)
