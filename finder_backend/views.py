5e71867d06bd7a474429415fc89999a60bdc9b60
Sriram Iyer <ramiyer1998@gmail.com>
2019-07-13 14:48:07 -0400

    Use serializers to create views

--- finder_backend/views.py
+++ finder_backend/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Flight
+from .serializers import FlightSerializer
 
 # Create your views here.
+
+
+class ListFlight(generics.ListCreateAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class DetailFlight(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
