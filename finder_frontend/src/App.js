af06ce4f19d40892e48edba5eac1358fdd57db3a
Sriram Iyer <ramiyer1998@gmail.com>
2019-07-13 15:38:25 -0400

    Connect React to Django and print out model objects

--- finder_frontend/src/App.js
+++ finder_frontend/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    flights: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch("http://127.0.0.1:8000/api/");
+      const flights = await res.json();
+      this.setState({
+        flights
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.flights.map(item => (
+          <div>
+            <h2>
+              {item.origin} -> {item.destination}
+            </h2>
+            <h3>Departure Date: {item.departure_date}</h3>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
