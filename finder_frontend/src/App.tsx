a3409903ae1a24ce549ae30488c9eaa785efd3eb
Sriram Iyer <ramiyer1998@gmail.com>
2019-07-15 20:04:42 -0400

    Reinitialize frontend with TypeScript

---
+++ finder_frontend/src/App.tsx
@@ -0,0 +1,45 @@
+// App.js
+import React, { Component } from 'react';
+import { isUndefined } from 'util';
+
+interface flight {
+  id: number,
+  origin: string,
+  destination: string,
+  departure_date: string,
+  return_date: string
+}
+class App extends Component {
+  state = {
+    flights: Array<flight>()
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/');
+      const flights = await res.json();
+      this.setState({
+        flights
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.flights.map(item  => (
+          <div>
+              <h1>{item.origin}</h1>
+              <h1>{item.destination}</h1>
+              <span>{item.departure_date}</span>
+              <span>{item.return_date}</span>
+          </div>
+          ))}
+      </div>
+    );
+  }
+}
+
+export default App;
