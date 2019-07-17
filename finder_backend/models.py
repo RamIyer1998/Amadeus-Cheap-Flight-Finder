52b148499b54963565a136a69e2b18e4eb0066f5
Sriram Iyer <ramiyer1998@gmail.com>
2019-07-16 20:15:04 -0400

    Edit models to include price/direct flight

--- finder_backend/models.py
+++ finder_backend/models.py
@@ -8,3 +8,5 @@ class Flight(models.Model):
     destination = models.CharField(max_length=3)
     departure_date = models.CharField(max_length=10)
     return_date = models.CharField(max_length=10, blank=True)
+    price = models.IntegerField(default=0)
+    direct_flight = models.BooleanField(default=True)
