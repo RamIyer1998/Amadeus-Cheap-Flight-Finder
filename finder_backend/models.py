64969ccbbfeab0475373216eb1a44526c0a7d3b3
Sriram Iyer <ramiyer1998@gmail.com>
2019-07-22 19:36:27 -0400

    Start working on roundtrip model

--- finder_backend/models.py
+++ finder_backend/models.py
@@ -9,3 +9,17 @@ class Flight(models.Model):
     departure_date = models.CharField(max_length=10)
     return_date = models.CharField(max_length=10, blank=True)
     price = models.IntegerField(default=0)
+
+
+class RoundTrip(models.Model):
+    first_origin = models.CharField(max_length=3)
+    first_destination = models.CharField(max_length=3)
+    first_departure_time = models.CharField(max_length=50)
+    first_arrival_time = models.CharField(max_length=50)
+
+    second_origin = models.CharField(max_length=3)
+    second_destination = models.CharField(max_length=3)
+    second_departure_time = models.CharField(max_length=50)
+    second_arrival_time = models.CharField(max_length=50)
+
+    total_price = models.IntegerField(default=0)
